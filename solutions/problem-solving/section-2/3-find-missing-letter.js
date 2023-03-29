a2322f5f619b5659e8c6d18f86f7bd130a3a33ea
Kyle McPhail <99103416+MrMcSnail@users.noreply.github.com>
2023-03-29 11:02:05 +0100

    missing letter solution

--- solutions/problem-solving/section-2/3-find-missing-letter.js
+++ solutions/problem-solving/section-2/3-find-missing-letter.js
@@ -1,10 +1,36 @@
 const { check, runTest, skipTest } = require("../../test-api/index.js");
 /*
-In this function, you need to find out which letter is missing. But you can't use a reference lookup table (i.e. no array or object with the whole alphabet in it) so you will have think outside the box!
+What is this asking me to do? 
+>>>>>
+In this function, you need to find out which letter is missing. 
 
+What other data or methods might I need to use to create this behaviour
+>>>>>
+But you can't use a reference lookup table (i.e. no array or object with the whole alphabet in it) so you will have think outside the box!
+
+What behaviours should the code have?
+What are the inputs?
+What are the outputs?
+>>>>>
 This function needs to take a list and needs to return the letter it is missing.
 
-You will always get a sorted array of consecutive letters, and it will always have exactly one letter missing. The length of the array will always be at least 2. The array will always contain letters in only one case.
+What are the inputs?
+>>>>>
+You will always get a sorted array of consecutive letters, 
+and it will always have exactly one letter missing. 
+The length of the array will always be at least 2. 
+The array will always contain letters in only one case.
+*/
+
+/*
+- take list as an array
+- returns an empty string if no letters are missing
+- find letter that is missing:
+  - iterate through the array
+  - keep track of the previous char
+  - find out if the current letter is the next in the sequence
+    - if its not, the missing letter will be the letter that comes before the current letter and after the previous letter. 
+    >>>>>>>>>>Sequencing of the alphabet in JS? Can it increment or decrement chars in JS?
 */
 
 function findMissingLetter(letters) {
@@ -20,6 +46,7 @@ function findMissingLetter(letters) {
 
     if (isNotNextLetter) {
       missingLetter = String.fromCodePoint(currentCharacterCode - 1);
+      return missingLetter;
     };
     
     previousLetter = letters[i];
@@ -28,6 +55,7 @@ function findMissingLetter(letters) {
 }
 
 console.log("findMissingLetter()");
+
 
 runTest("returns an empty string if no letters are missing", function () {
   check(findMissingLetter(["A", "B", "C", "D", "E"])).isEqualTo("");
